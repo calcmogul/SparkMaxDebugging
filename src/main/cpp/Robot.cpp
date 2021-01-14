b369694f6a082683c648275e8db06f96a3ac2784
Tyler Veness <calcmogul@gmail.com>
2021-01-13 19:47:51 -0800

    Add HAL Notifier API stress test

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 FRC Team 3512. All Rights Reserved.
+// Copyright (c) 2020-2021 FRC Team 3512. All Rights Reserved.
 
 #include <frc/TimedRobot.h>
 #include <rev/CANSparkMax.h>
@@ -22,5 +22,25 @@ int main(int argc, char** argv) {
     return RUN_ALL_TESTS();
 }
 
-TEST(RobotTest, Init) { Robot robot; }
+TEST(RobotTest, Init) {
+    uint64_t kThreadRate = 25000;
+    int32_t status = 0;
+    uint64_t currTime;
+    uint64_t startTime = HAL_GetFPGATime(&status);
+
+    HAL_NotifierHandle m_setNotifier = HAL_InitializeNotifier(&status);
+
+    for (int i = 0; i < 1000; i++) {
+        currTime = HAL_GetFPGATime(&status);
+        uint64_t nextTime = static_cast<uint64_t>(
+            currTime + (kThreadRate - (currTime - startTime)));
+        HAL_UpdateNotifierAlarm(m_setNotifier, nextTime, &status);
+        if (HAL_WaitForNotifierAlarm(m_setNotifier, &status) == 0) {
+            break;
+        }
+        startTime = HAL_GetFPGATime(&status);
+    }
+    HAL_StopNotifier(m_setNotifier, &status);
+    HAL_CleanNotifier(m_setNotifier, &status);
+}
 #endif
